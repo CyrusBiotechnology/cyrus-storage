3242d48046294ceaa1d01823d42de5e565ff0495
Javier Castellanos <javierbq@gmail.com>
2016-06-01 16:24:59 -0700

    Fixed defaults in local storage.

--- local.js
+++ local.js
@@ -7,13 +7,14 @@ var _ = require('lodash');
 var assert = require('assert');
 
  module.exports.init = function(config) {
-   config = config || {};
-   config.dir = config.dir || './local_storage';
+   var _config = _.merge(config, {
+     dir: './local_storage'
+   });
 
     return {
         store: function(data, dir, callback) {
             var callback = callback || dir;
-            var dir = (typeof(dir) == 'function')? config.dir : dir;
+            var dir = (typeof(dir) == 'function')? _config.dir : dir;
             assert.ok(callback);
             assert.ok(dir);
 
@@ -21,10 +22,10 @@ var assert = require('assert');
                 fs.mkdirSync(dir);
             }
 
-            var extension = config.extension + (config.compression) ? 'gz' : '';
+            var extension = _config.extension + (_config.compression) ? 'gz' : '';
             var path = dir + '/' + uuid.v4() + extension;
             var writeStream = fs.createWriteStream(path);
-            streamUtils.writeToStorageStream(data, writeStream, config.compression, () => {
+            streamUtils.writeToStorageStream(data, writeStream, _config.compression, () => {
               callback(path);
             });
         },
