316f39c9c583519f0fcdb2623b83a34ded2a94c4
Javier Castellanos <javierbq@gmail.com>
2016-06-02 02:14:26 -0700

    fixed local backend.

--- local.js
+++ local.js
@@ -20,13 +20,28 @@ var assert = require('assert');
             var bucketDir = ((typeof(bucketDir) == 'function') || (bucketDir === undefined)) ? _config.bucketDir : bucketDir;
             var extension = _config.extension + ((_config.compression)? '.gz' : '');
             var filename = ((typeof(filename) == 'function') || (filename === undefined)) ?  (uuid.v4() + extension) : filename;
-	    var path = ((dir)? (dir + '/') : '') + filename; 
+            var splitPath = [bucketDir, dir, filename];
+            var path = _.join(splitPath, '/');
             assert.ok(callback, 'callback missing.');
             assert.ok(bucketDir, 'missing bucket dir');
 
-            var extension = _config.extension + (_config.compression) ? 'gz' : '';
-            var path = dir + '/' + uuid.v4() + extension;
-            var writeStream = fs.createWriteStream(path);
+            console.log('path: ', path);
+
+            _(_.map(
+              _.range(splitPath.length-1),
+              (i) => {return _.join(_.slice(splitPath,0,i+1), '/')}
+
+            )).forEach( (folder) => {
+              console.log('- ', folder);
+
+              if (!fs.existsSync(folder)) {
+                console.log('~ ', folder);
+                fs.mkdirSync(folder);
+              }
+            });
+
+
+            var writeStream = fs.createWriteStream('./' + path);
             streamUtils.writeToStorageStream(data, writeStream, _config.compression, () => {
               callback(path);
             });
